9273e9ec07b1a74190ea47bdd14fc79b0accead4
Vishal Bhat Soori <vishalbhatsoori@gmail.com>
2026-01-08 10:56:30 +0000

    added a testing route for doing stress testing

--- backend/src/routes/ai.routes.js
+++ backend/src/routes/ai.routes.js
@@ -1,9 +1,20 @@
 import express from 'express';
-import {controllerPostCode } from '../controllers/ai.controller.js';
+import { controllerPostCode } from '../controllers/ai.controller.js';
 
 const router = express.Router();
 
 router.post('/post-code',controllerPostCode);
+
+// this is a temporary route for stress testing
+router.post('/stress-test', async (req, res) => {
+    // 1. Simulate the delay of AI (e.g., 2 seconds)
+    // This keeps the connection open, occupying your server's RAM/CPU connection slot
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    res.send({
+        "review": "This is a FAKE review for stress testing. Your server handled this perfectly! 🚀"
+    });
+});
 
 export default router;
 
