1ccec79676b893feb383152f37a16b1a81114c78
Vishal Bhat Soori <vishalbhatsoori@gmail.com>
2026-01-27 11:20:58 +0000

    added try catch to handle any errors gracefully in controller

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,12 +1,21 @@
-import {generateFromPrompt as aiService} from '../services/ai.services.js';
 
 async function controllerPostCode(req,res){
-    const code = req.body.code;
-    if(!code){
-        return res.status(400).send("Code is required please");
+    try {
+        const code = req.body.code;
+        if (!code) {
+            return res.status(400).send("Code is required please");
+        }
+        const response = await aiService(code);
+        if (response.includes("The LLM is currently busy")) {
+            return res.status(503).send(response);
+        }
+
+        res.send(response);
+    } catch (error) {
+        console.error("Controller Error:", error.message);
+        res.status(500).send("Internal Server Error");
     }
-    const response = await aiService(code);
-    res.send(response);
 }
 
-export {controllerPostCode};
+export { controllerPostCode };
+
